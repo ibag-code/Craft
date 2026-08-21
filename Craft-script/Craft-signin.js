4b63cada579bc55bf7971a7b198bc720291dd87a
ibag <ibagdesignengineer@gmail.com>
2026-08-21 10:58:52 +0100

    Sign-Up and sign in code

---
+++ Craft-script/Craft-signin.js
@@ -0,0 +1,63 @@
+
+const signForm = document.getElementById("form");
+const emailAddress = document.getElementById("email");
+const password = document.getElementById("password");
+
+
+signForm.addEventListener("submit", (event) => {
+   event.preventDefault();
+
+  const emailValue = emailAddress.value.trim();
+  const passwordValue = password.value.trim();
+
+  emailAddress.classList.remove("wrong");
+  password.classList.remove("wrong");
+
+  if (emailValue === "") {
+    emailAddress.classList.add("wrong");
+    alert("Email is required");
+    return;
+  }
+
+  if (passwordValue === "") {
+    password.classList.add("wrong");
+    alert("Password is required");
+    return;
+  }
+
+  if (passwordValue.length < 8) {
+    password.classList.add("wrong");
+    alert("Your password must be at least 8 characters");
+    return;
+  }
+
+  if (!emailValue.includes("@")) {
+    emailAddress.classList.add("wrong");
+    alert("Wrong email address");
+    return;
+  }
+
+
+
+  const users = JSON.parse(localStorage.getItem("users")) || [];
+
+  const user = users.find((u) => {
+    return u.email === emailValue && u.password === passwordValue;
+  });
+
+  if (user) {
+    localStorage.setItem("loggedInUser", JSON.stringify(user));
+
+    alert("Login successful!");
+
+    setTimeout(() => {
+      window.location.href = "Craft-website.html";
+    }, 1000);
+  } else {
+    emailAddress.classList.add("wrong");
+    password.classList.add("wrong");
+    alert("Invalid email or password");
+  }
+
+   
+});
--- Craft-script/Craft-signup.js
+++ Craft-script/Craft-signup.js
@@ -1,27 +1,42 @@
 
-
-
-
-
-const formPage = document.getElementById('sign-up-form')
+const formPage = document.getElementById('sign-up-form');
+const firstName = document.getElementById("first-name")
+const lastName = document.getElementById("last-name");
+const  emailAddress = document.getElementById("email")
+const password = document.getElementById("password");
+const rePassword = document.getElementById("re-enter-password")
 
 
 
 formPage.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const firstName = document.getElementById("first-name")
-    const lastName = document.getElementById("last-name");
-    const  emailAddress = document.getElementById("email")
-    const password = document.getElementById("password");
-    const rePassword = document.getElementById("re-enter-password")
 
-    // alert('All fields are required.');
+    //All value in the input field
+    const fname = firstName.value.trim();
+    const lname = lastName.value.trim();
+    const emailVal = emailAddress.value.trim();
+    const pword = password.value.trim();
+    const rePword = rePassword.value.trim();
 
-    if (!firstName.value.trim() || !lastName.value.trim() ||!emailAddress.value.trim() || !password.value.trim() || !rePassword.value.trim()) {
-        alert('All fields are required.');
+    // if (!firstName.value.trim() || !lastName.value.trim() ||!emailAddress.value.trim() || !password.value.trim() || !rePassword.value.trim()) {
+    //     alert('All fields are required Sir');
+    //     return;
+    // }
+
+    if (fname === "") {
+
+        alert('All fields are required Sir');
+        firstName.classList.add('wrong')
         return;
     }
+    if (lname === "") {
+
+        alert('All fields are required Sir');
+        lastName.classList.add('wrong')
+        return;
+    }
+
 
     if (password.value.length < 8) {
           alert('Password must be at least 8 characters long.');
@@ -39,11 +54,19 @@ formPage.addEventListener("submit", (event) => {
 
 
     if (!emailAddress.value.includes('@')) {
+        
         alert("Wrong Email Address");
         emailAddress.classList.add('wrong')
         return;
     }
 
+
+    if (!emailAddress.value.includes('@gmail.com')) {
+        
+        alert("Your email address does not have include");
+        emailAddress.classList.add('wrong')
+        return;
+    }
 
 
       const users = JSON.parse(localStorage.getItem('users')) || [];
@@ -58,16 +81,18 @@ formPage.addEventListener("submit", (event) => {
       alert('User signed up successfully!');
 
       setTimeout(() => {
+        //meant to be the dashboard
           window.location.href = 'Craft-website.html';
       }, 1000);
+      
 })
 
-
-const allInput = [ firstName, lastName, emailAddress, password, rePassword];
+//remove erroor
+const allInput = [firstName, lastName, emailAddress, password, rePassword];
 
 allInput.forEach(input => {
-    input.addEventListener('input', () => {
-        if (input.classList.contain('wrong')) {
+    input.addEventListener('click', () => {
+        if (input.classList.contains('wrong')) {
             input.classList.remove('wrong');
             input.value = "";
         }
